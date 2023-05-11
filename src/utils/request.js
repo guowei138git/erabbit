0306ff8891c0ddfb18eed0437d957997f918cf74
huaheshang <908592508@qq.com>
2023-05-11 08:10:46 +0800

    请求工具函数

--- src/utils/request.js
+++ src/utils/request.js
@@ -43,24 +43,24 @@ instance.interceptors.response.use((res) => {
         // 备注：router.currentRoute 是ref响应式数据
         const fullPath = router.currentRoute.value.fullPath
         // 转换uri编码 - 防止解析地址出问题
-        const url =  encodeURIComponent(fullPath)
+        const url = encodeURIComponent(fullPath)
         // 4.跳转到登录页面
         router.push('/login?redirectUrl=' + url)
     }
     return Promise.reject(err)
 })
 
-
 // 导出一个函数 - 请求工具函数
 export default (url, method, submitData) => {
     // 负责发请求：请求地址，请求方式，提交的数据
     return instance({
-        url,
-        method,
+        url, // 请求地址
+        method, // 请求方式
         // 1. 如果是get请求  需要使用params来传递submitData   ?a=10&c=10
-        // 2. 如果不是get请求  需要使用data来传递submitData   请求体传参
+        // 2. 如果不get请求  需要使用data来传递submitData   请求体传参
         // [] 设置一个动态的key, 写js表达式，js表达式的执行结果当作KEY
-        // method参数：get,Get,GET  转换成小写再来判断
+        // []: submitData  // 提交的数据 
+        // method参数：get,Get,GET  转换成小写再来判断 - toLowerCase()
         // 在对象，['params']:submitData ===== params:submitData 这样理解
         [method.toLowerCase() === 'get' ? 'params' : 'data']: submitData
     })
